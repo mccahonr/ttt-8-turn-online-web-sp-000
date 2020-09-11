366b4aeee5f56256c88259f2cd836e7801917d2f
Ryan McCahon <mccahonr@yahoo.com>
2020-09-11 15:58:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,3 +11,11 @@ def input_to_index(user_input)
   index -= 1
   index
 end
+
+def valid_move?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    true
+  else
+    false
+  end
+end
